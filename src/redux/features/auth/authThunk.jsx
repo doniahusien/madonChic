281bcf1cddfb71a7ac671f4bc25194233c8ce4ff
doniahusien <doniahusien@gmail.com>
2025-02-20 23:46:26 +0200

    🔄 Add Redux Thunk functions for authentication

---
+++ src/redux/features/auth/authThunk.jsx
@@ -0,0 +1,11 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const login = createAsyncThunk(("auth/login", async (formData, { rejectWithValue }) => {
+    try {
+        const response = await axios.post("api/", formData);
+        return response.data;
+    } catch (error) {
+        return rejectWithValue(error.response?.data || "Login failed");
+    }
+}))
