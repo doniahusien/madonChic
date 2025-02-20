afa9a0877a92cf721554c0ade7c52ef4a96ce255
doniahusien <doniahusien@gmail.com>
2025-02-20 23:46:14 +0200

    🚀 Add signup page for authentication

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
-const page = () => {
+import AuthWrapper from '@/components/auth/AuthWrapper'
+import SignupForm from '@/components/auth/SignupForm'
+import imgSrc from '../../../../public/signup.jpg'
+import Link from 'next/link'
+const signupPage = () => {
   return (
-    <div>page</div>
+    <AuthWrapper imgSrc={imgSrc} header="Signup" signup>
+      <SignupForm />
+      <h2 className="text-center mt-5 text-gray-600">
+        Have an account?{" "}
+        <Link href="login"><span className="text-blue-600 hover:underline cursor-pointer">Login</span></Link>
+      </h2>
+    </AuthWrapper>
   )
 }
 
-export default page
+export default signupPage
