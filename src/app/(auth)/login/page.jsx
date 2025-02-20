de191ec611191ecead0a706238c260ed37e834a0
doniahusien <doniahusien@gmail.com>
2025-02-20 23:46:13 +0200

    🚀 Add login page for authentication

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import imgSrc from '../../../../public/login.png';
+import Link from 'next/link';
+import LoginForm from '@/components/auth/LoginForm';
+import AuthWrapper from '@/components/auth/AuthWrapper';
+const LoginPage = () => {
+    return (
+        <AuthWrapper imgSrc={imgSrc} header="Login">
+                <LoginForm />
+                <h2 className="text-center mt-5 text-gray-600">
+                    Don't have an account?{" "}
+                    <Link href="signup"><span className="text-blue-600 hover:underline cursor-pointer">Sign Up</span></Link>
+                </h2>
+        </AuthWrapper>
+    );
+};
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
-
-export default page
+export default LoginPage;
