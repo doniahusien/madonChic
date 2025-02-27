df7474ce349755b09a1e1a7cd4bd4ec6cacf43eb
doniahusien <doniahusien@gmail.com>
2025-02-27 14:06:28 +0200

    Fix: Updated page logic in perview/[id]/page.jsx

--- src/app/perview/[id]/page.jsx
+++ src/app/perview/[id]/page.jsx
@@ -4,31 +4,38 @@ import ProductReviews from "@/components/preview/ProductReviews";
 import DeliverySection from "@/components/preview/DeliverySection";
 export default function ProductPreview({ params }) {
     const { id } = params;
-    const product =
-        {
-            id: "1",
-            name: "Men Solid Slim Fit Mid-Rise Formal Trousers",
-            price: 999,
-            originalPrice: 2298,
-            discount: 57,
-            sizes: ["XS", "S", "M", "L", "XL"],
-            images: [
-                "/preview.jpeg",
-                "/preview.jpeg",
-                "/preview.jpeg"
-            ],
-            description:
-                "Navy blue woven formal Slim fit Mid-rise length, regular pattern solid flat-front, with no pleats design, feature: plain, 4 pockets.",
-            reviews: [],
-        }
-    return (
+    const product = {
+        id: "1",
+        name: "Men Solid Slim Fit Mid-Rise Formal Trousers",
+        price: 999,
+        originalPrice: 2298,
+        discount: 57,
+        sizes: ["XS", "S", "M", "L", "XL"],
+        images: [
+            "/preview.jpeg",
+            "/preview.jpeg",
+            "/preview.jpeg"
+        ],
+        description:
+            "Navy blue woven formal Slim fit Mid-rise length, regular pattern solid flat-front, with no pleats design, feature: plain, 4 pockets.",
+        reviews: [
+            {
+                name: "Ahmed Abdelfattah",
+                avatar: "/preview.jpeg",
+                rating: 4,
+                comment: "high quilty",
+                date: "16 Feb, 2025"
+            }
+        ],
+    };
 
+    return (
         <div className="flex flex-col justify-center md:justify-start gap-5 pt-5 px-2 md:px-10 bg-gray-100">
             <div className="flex flex-col justify-center md:justify-start gap-5 md:flex-row">
                 <ProductImages images={product.images} />
                 <ProductDetails product={product} />
             </div>
-            <ProductReviews review={product.reviews}/>
+            <ProductReviews reviews={product.reviews} />
             <DeliverySection />
         </div>
     );
