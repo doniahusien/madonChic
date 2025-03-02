cf0f92dcb7d6783731ea73947ce638360f3f980e
doniahusien <doniahusien@gmail.com>
2025-03-02 19:29:43 +0200

    Fix: Improve login page UI and authentication logic

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -3,15 +3,18 @@ import imgSrc from '../../../../public/login.png';
 import Link from 'next/link';
 import LoginForm from '@/components/auth/LoginForm';
 import AuthWrapper from '@/components/auth/AuthWrapper';
+import GuestRoute from '@/components/GuestRoute';
 const LoginPage = () => {
     return (
+        <GuestRoute>
         <AuthWrapper imgSrc={imgSrc} header="Login">
                 <LoginForm />
                 <h2 className="text-center mt-5 text-gray-600">
                     Don't have an account?{" "}
                     <Link href="/signup"><span className="text-blue-600 hover:underline cursor-pointer">Sign Up</span></Link>
                 </h2>
-        </AuthWrapper>
+            </AuthWrapper>
+        </GuestRoute>
     );
 };
 
