d3887b69fbf8dcc4cf91645aee0538bcfb788dc0
doniahusien <doniahusien@gmail.com>
2025-03-02 19:29:13 +0200

    Fix: Update profile data display and logout handling

--- src/components/profile/ProfileData.jsx
+++ src/components/profile/ProfileData.jsx
@@ -1,6 +1,18 @@
+"use client"
 import React from 'react'
-
+import { logout } from '@/redux/features/auth/authThunk'
+import { useSelector, useDispatch } from 'react-redux'
+import { useRouter } from 'next/navigation'
 const ProfileData = (profileData, handleImageChange,selectedImage) => {
+    const dispatch = useDispatch();
+    const router =useRouter()
+    const handleLogout = async() => {
+        const result = await dispatch(logout());
+        if (logout.fulfilled.match(result)) {
+            router.push("/login");
+            console.log("Logout Success")
+        }
+    }
     return (
         <div className="flex items-center gap-6 border-b pb-4">
             <div className="relative w-24 h-24 bg-gray-200 rounded-full flex items-center justify-center overflow-hidden">
@@ -22,7 +34,9 @@ const ProfileData = (profileData, handleImageChange,selectedImage) => {
                 </h3>
                 <p className="text-gray-600">Email - {profileData.email}</p>
                 <p className="text-gray-600">Mobile No. - {profileData.mobile}</p>
-                <button className="mt-2 px-4 py-2 bg-gray-300 text-black rounded hover:bg-gray-400">
+                <button
+                    onClick={handleLogout}
+                    className="mt-2 px-4 py-2 bg-gray-300 text-black rounded hover:bg-gray-400">
                     Logout
                 </button>
             </div>
