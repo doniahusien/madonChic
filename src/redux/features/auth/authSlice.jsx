7014d120856ba5dcba9d8dff795f56d48c315322
doniahusien <doniahusien@gmail.com>
2025-03-02 19:29:14 +0200

    Fix: Improve Redux state management for authentication

--- src/redux/features/auth/authSlice.jsx
+++ src/redux/features/auth/authSlice.jsx
@@ -1,9 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { login } from "./authThunk";
+import { login, signup ,logout} from "./authThunk";
+import Cookies from "js-cookie";
 const initialState = {
     loading: false,
-    token: null,
+    token: Cookies.get("token") || null,
     error: null,
+    message: null
 }
 const authSlice = createSlice({
     name: "auth",
@@ -12,14 +14,40 @@ const authSlice = createSlice({
     extraReducers: (builder) => {
         builder.addCase(login.pending, (state) => {
             state.loading = true;
-        }).addCase(login.fulfilled, (state,action) => {
+        }).addCase(login.fulfilled, (state, action) => {
             state.token = action.payload.token;
-            state.loading = true;
+            state.loading = false;
             state.error = false;
+            Cookies.set("token", action.payload.token, { expires: 5});
         }).addCase(login.rejected, (state, action) => {
             state.loading = false;
             state.error = action.payload.error;
         })
+            //signup
+            .addCase(signup.pending, (state) => {
+                state.loading = true;
+            }).addCase(signup.fulfilled, (state, action) => {
+                state.token = action.payload.token;
+                state.loading = false;
+                state.error = false;
+                Cookies.set("token", action.payload.token);
+            }).addCase(signup.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload.errors;
+            })
+        //logout 
+        .addCase(logout.pending, (state) => {
+            state.loading = true;
+        }).addCase(logout.fulfilled, (state, action) => {
+            state.loading = false;
+            state.message = action.payload.message;
+            state.token = null;
+            Cookies.remove("token");
+        }).addCase(logout.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload.error;
+        })
+
     }
 })
 export default authSlice.reducer
