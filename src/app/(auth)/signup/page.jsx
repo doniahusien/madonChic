458d1a21f385fe68ba36112f53c73e79472bf774
doniahusien <doniahusien@gmail.com>
2025-03-02 19:29:44 +0200

    Fix: Enhance signup validation and error handling

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -3,15 +3,18 @@ import AuthWrapper from '@/components/auth/AuthWrapper'
 import SignupForm from '@/components/auth/SignupForm'
 import imgSrc from '../../../../public/signup.jpg'
 import Link from 'next/link'
+import GuestRoute from '@/components/GuestRoute'
 const signupPage = () => {
   return (
-    <AuthWrapper imgSrc={imgSrc} header="Signup" signup>
-      <SignupForm />
-      <h2 className="text-center mt-5 text-gray-600">
-        Have an account?{" "}
-        <Link href="/login"><span className="text-blue-600 hover:underline cursor-pointer">Login</span></Link>
-      </h2>
-    </AuthWrapper>
+    <GuestRoute>
+      <AuthWrapper imgSrc={imgSrc} header="Signup" signup>
+        <SignupForm />
+        <h2 className="text-center mt-5 text-gray-600">
+          Have an account?{" "}
+          <Link href="/login"><span className="text-blue-600 hover:underline cursor-pointer">Login</span></Link>
+        </h2>
+      </AuthWrapper>
+    </GuestRoute>
   )
 }
 
