eff8565cac10a052d85a969ff5abe5d0c559a700
doniahusien <doniahusien@gmail.com>
2025-03-05 14:20:07 +0200

    refactor(store): update store configuration

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './features/auth/authSlice'
 import homeReducer from './features/home/homeSlice'
+import shopReducer from './features/shop/shopSlice'
 const store = configureStore({
     reducer: {
         auth: authReducer,
         home: homeReducer,
+        shop: shopReducer,
     }
 })
 export default store;
