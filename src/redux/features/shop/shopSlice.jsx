c7085ea396e6306874e7527370a61cd8c85e47a9
doniahusien <doniahusien@gmail.com>
2025-03-05 17:20:53 +0200

    refactor(shop): update Redux slice for better state handling

--- src/redux/features/shop/shopSlice.jsx
+++ src/redux/features/shop/shopSlice.jsx
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchProduct,fetchProductReview } from "./shopThunk";
+import { addProductReview, fetchProduct, fetchProductReview } from "./shopThunk";
 const initialState = {
     product: [],
-    review:[],
+    review: [],
     loading: false,
     error: null,
+    success: null,
 }
 const shopSlice = createSlice({
     name: "shop",
@@ -15,25 +16,33 @@ const shopSlice = createSlice({
             state.product = action.payload;
             state.loading = false;
         })
-        .addCase(fetchProduct.pending, (state) => {
-            state.loading = true;
-        })
-        .addCase(fetchProduct.rejected, (state, action) => {
-            state.error = action.payload;
-            state.loading = false;
-        })
-        //product review 
-        .addCase(fetchProductReview.fulfilled, (state, action) => {
-            state.review = action.payload;
-            state.loading = false;
-        })
-        .addCase(fetchProductReview.pending, (state) => {
-            state.loading = true;
-        })
-        .addCase(fetchProductReview.rejected, (state, action) => {
-            state.error = action.payload;
-            state.loading = false;
-        })
+            .addCase(fetchProduct.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(fetchProduct.rejected, (state, action) => {
+                state.error = action.payload;
+                state.loading = false;
+            })
+            //product review 
+            .addCase(fetchProductReview.fulfilled, (state, action) => {
+                state.review = action.payload;
+                state.loading = false;
+            })
+            .addCase(fetchProductReview.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(fetchProductReview.rejected, (state, action) => {
+                state.error = action.payload;
+                state.loading = false;
+            })
+            //add review 
+            .addCase(addProductReview.fulfilled, (state, action) => {
+                state.success = action.payload.message;
+                state.loading = false;
+            })
+            .addCase(addProductReview.rejected, (state, action) => {
+                state.error = action.payload;
+            });
     }
 });
 export default shopSlice.reducer;
