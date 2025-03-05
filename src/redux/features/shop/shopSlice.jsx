b1dbc604ad0aded1124368559d5517e1bd1bf0b4
doniahusien <doniahusien@gmail.com>
2025-03-05 16:08:50 +0200

    Refactor shopSlice for better state management

--- src/redux/features/shop/shopSlice.jsx
+++ src/redux/features/shop/shopSlice.jsx
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchProduct } from "./shopThunk";
+import { fetchProduct,fetchProductReview } from "./shopThunk";
 const initialState = {
     product: [],
+    review:[],
     loading: false,
     error: null,
 }
@@ -18,6 +19,18 @@ const shopSlice = createSlice({
             state.loading = true;
         })
         .addCase(fetchProduct.rejected, (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        })
+        //product review 
+        .addCase(fetchProductReview.fulfilled, (state, action) => {
+            state.review = action.payload;
+            state.loading = false;
+        })
+        .addCase(fetchProductReview.pending, (state) => {
+            state.loading = true;
+        })
+        .addCase(fetchProductReview.rejected, (state, action) => {
             state.error = action.payload;
             state.loading = false;
         })
