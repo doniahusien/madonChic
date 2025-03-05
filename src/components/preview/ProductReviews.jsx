7f8125b5a8bc9bc890677cfae62d57e4a800c9bc
doniahusien <doniahusien@gmail.com>
2025-03-05 16:08:49 +0200

    Enhance product reviews section

--- src/components/preview/ProductReviews.jsx
+++ src/components/preview/ProductReviews.jsx
@@ -3,42 +3,32 @@ import React, { useState } from "react";
 import Image from "next/image";
 import ReviewModel from "./ReviewModel";
 
-const ProductReviews = ({ reviews }) => {
-  const [showModal, setShowModal] = useState(false);
+const ProductReviews = ({ review ,setShowModal,showModal}) => {
 
   return (
     <div className="p-4">
-      <div className="flex w-full md:w-1/2 justify-between items-center border-b pb-2">
-        <h3 className="text-lg font-semibold">
-          Total Reviews {reviews.length}
-        </h3>
-        <button
-          className="bg-red-600 text-white px-4 py-2 rounded-md"
-          onClick={() => setShowModal(true)}
-        >
-          Write Review
-        </button>
-      </div>
+      
 
       {/* Displaying Reviews */}
-      {reviews.map((rev, index) => (
+      {review.map((rev, index) => (
         <div key={index} className="flex items-start space-x-5 mt-4 border-b pb-3 w-full md:w-1/2">
           <Image
-            src={rev.avatar}
+            src={rev.avatar||"/login.png"}
             alt="User Avatar"
             width={40}
             height={40}
             className="rounded-full"
           />
           <div className="flex-1">
-            <p className="font-thin text-sm md:font-semibold md:text-base ">{rev.name}</p>
+            <p className="font-thin text-sm md:font-semibold md:text-base ">
+              {rev.user.first_name} {rev.user.last_name}
+            </p>
             <div className="flex">
               {[1, 2, 3, 4, 5].map((star) => (
                 <span
                   key={star}
-                  className={`text-lg ${
-                    star <= rev.rating ? "text-yellow-400" : "text-gray-300"
-                  }`}
+                  className={`text-lg ${star <= rev.stars ? "text-yellow-400" : "text-gray-300"
+                    }`}
                 >
                   ★
                 </span>
@@ -46,7 +36,13 @@ const ProductReviews = ({ reviews }) => {
             </div>
             <p>{rev.comment}</p>
           </div>
-          <p className="text-gray-500 text-sm">{rev.date}</p>
+          <p className="text-gray-500 text-sm">
+            {new Date(rev.created_at).toLocaleDateString("en-US", {
+              day: "2-digit",
+              month: "short",
+              year: "numeric",
+            })}
+          </p>
         </div>
       ))}
 
