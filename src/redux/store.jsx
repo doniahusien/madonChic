54bd98ee0e7df1a903a8f550622b573d0894f1e6
doniahusien <doniahusien@gmail.com>
2025-03-06 16:04:44 +0200

    Update Redux store: Add cart reducers and state handling

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -3,12 +3,14 @@ import authReducer from './features/auth/authSlice'
 import homeReducer from './features/home/homeSlice'
 import shopReducer from './features/shop/shopSlice'
 import profileReducer from './features/profile/profileSlice'
+import cartReducer from './features/cart/cartSlice'
 const store = configureStore({
     reducer: {
         auth: authReducer,
         home: homeReducer,
         shop: shopReducer,
         profile: profileReducer,
+        cart: cartReducer,
     }
 })
 export default store;
