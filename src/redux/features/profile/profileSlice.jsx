b4afb069188833e057c42df46fd1107ac352f49a
doniahusien <doniahusien@gmail.com>
2025-03-07 23:08:15 +0200

    Refactor: Improve profile state management

--- src/redux/features/profile/profileSlice.jsx
+++ src/redux/features/profile/profileSlice.jsx
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchProfile,updateProfile } from "./profileThunk";
+import { fetchProfile,updateProfile,uploadImage } from "./profileThunk";
 const initialState = {
     profile: {},
     error: null,
@@ -22,15 +22,20 @@ const profileSlice = createSlice({
             state.loading = false;
             state.error = action.error.message;
         })
-        .addCase(updateProfile.pending, (state, action) => {
-            state.loading = true;
-            state.error = null;
-        }).addCase(updateProfile.fulfilled, (state, action) => {
-            state.loading = false;
-        }).addCase(updateProfile.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.error.message;
-    })
+            .addCase(updateProfile.pending, (state, action) => {
+                state.loading = true;
+                state.error = null;
+            }).addCase(updateProfile.fulfilled, (state, action) => {
+                state.loading = false;
+            }).addCase(updateProfile.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            })
+            .addCase(uploadImage.fulfilled, (state, action) => {
+                console.log("done");
+                ;
+            }); 
+        
     }
 });
 export default profileSlice.reducer;
