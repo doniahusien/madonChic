cef4850f77af47ae4027091579f83d3730f0f83d
doniahusien <doniahusien@gmail.com>
2025-03-11 00:51:45 +0200

    fix login to be dynamic

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
-import imgSrc from '../../../../public/login.png';
-import Link from 'next/link';
-import LoginForm from '@/components/auth/LoginForm';
-import AuthWrapper from '@/components/auth/AuthWrapper';
-import GuestRoute from '@/components/GuestRoute';
+"use client";
+
+import React from "react";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+import AuthWrapper from "@/components/auth/AuthWrapper";
+import GuestRoute from "@/components/GuestRoute";
+import imgSrc from "../../../../public/login.png";
+
+const LoginForm = dynamic(() => import("@/components/auth/LoginForm"), { ssr: false });
+
 const LoginPage = () => {
-    return (
-        <GuestRoute>
-        <AuthWrapper imgSrc={imgSrc} header="Login">
-                <LoginForm />
-                <h2 className="text-center mt-5 text-gray-600">
-                    Don't have an account?{" "}
-                    <Link href="/signup"><span className="text-blue-600 hover:underline cursor-pointer">Sign Up</span></Link>
-                </h2>
-            </AuthWrapper>
-        </GuestRoute>
-    );
+  return (
+    <GuestRoute>
+      <AuthWrapper imgSrc={imgSrc} header="Login">
+        <LoginForm />
+        <h2 className="text-center mt-5 text-gray-600">
+          Don't have an account?{" "}
+          <Link href="/signup">
+            <span className="text-blue-600 hover:underline cursor-pointer">Sign Up</span>
+          </Link>
+        </h2>
+      </AuthWrapper>
+    </GuestRoute>
+  );
 };
 
 export default LoginPage;
